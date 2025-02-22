f9c6875d66e7bea5b7810a1c87ebc3d20172a171
Ali Abbasov <ali3abbasov@gmail.com>
2025-02-22 11:56:48 +0400

    refactor: Add todo task for talk.ps1

--- server/static/scripts/talk.ps1
+++ server/static/scripts/talk.ps1
@@ -1,3 +1,5 @@
+# TODO: Make this accept a parameter for the text to speak (Like with toast.ps1)
+
 Function Speak-Text($Text) { Add-Type -AssemblyName System.speech; $TTS = New-Object System.Speech.Synthesis.SpeechSynthesizer; $TTS.Speak($Text) }
 Speak-Text "Hello World!"
 Exit
